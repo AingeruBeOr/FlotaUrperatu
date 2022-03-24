768b60fa22e3f030a79199f857d0b09ee8a0742d
Aingeru <1405bellido@gmail.com>
2022-03-24 18:34:37 +0100

    asdf

--- src/bista/Menu.java
+++ src/bista/Menu.java
@@ -6,8 +6,12 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.Font;
 
-public class proba extends JFrame {
+public class Menu extends JFrame {
 
 	private JPanel contentPane;
 
@@ -18,7 +22,7 @@ public class proba extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					proba frame = new proba();
+					Menu frame = new Menu();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -30,13 +34,27 @@ public class proba extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public proba() {
+	public Menu() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 405, 439);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JPanel north = new JPanel();
+		contentPane.add(north, BorderLayout.NORTH);
+		
+		JLabel JokoIzen = new JLabel("FLOTA URPERATU");
+		JokoIzen.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		north.add(JokoIzen);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(new GridLayout(5, 1, 0, 0));
+		
+		JButton botoi1 = new JButton("Jokoa hasi");
+		panel.add(botoi1);
 	}
 
 }
---
+++ src/eredua/FlotaUrperatu.java
@@ -0,0 +1,12 @@
+package eredua;
+
+public class FlotaUrperatu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+
+}
