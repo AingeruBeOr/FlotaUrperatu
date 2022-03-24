40f2e92d47bb8280de26ac4c65d1356b1be98af2
Aingeru <1405bellido@gmail.com>
2022-03-24 18:53:55 +0100

    asdf

--- src/bista/Menu.java
+++ src/bista/Menu.java
@@ -35,13 +35,17 @@ public class Menu extends JFrame {
 	 * Create the frame.
 	 */
 	public Menu() {
+		initialize();
+	}
+	
+	private void initialize() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 405, 439);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		
+	
 		JPanel north = new JPanel();
 		contentPane.add(north, BorderLayout.NORTH);
 		
@@ -49,12 +53,29 @@ public class Menu extends JFrame {
 		JokoIzen.setFont(new Font("Tahoma", Font.PLAIN, 30));
 		north.add(JokoIzen);
 		
-		JPanel panel = new JPanel();
-		contentPane.add(panel, BorderLayout.CENTER);
-		panel.setLayout(new GridLayout(5, 1, 0, 0));
+		JPanel center = new JPanel();
+		contentPane.add(center, BorderLayout.CENTER);
+		center.setLayout(new GridLayout(5, 1, 0, 0));
 		
 		JButton botoi1 = new JButton("Jokoa hasi");
-		panel.add(botoi1);
+		botoi1.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		center.add(botoi1);
+		
+		JButton botoi2 = new JButton("Ranking ikusi");
+		botoi2.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		center.add(botoi2);
+		
+		JButton botoi3 = new JButton("Partida kargatu");
+		botoi3.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		center.add(botoi3);
+		
+		JButton botoi4 = new JButton("Arauak ikusi");
+		botoi4.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		center.add(botoi4);
+		
+		JButton botoi5 = new JButton("Jokoa itxi");
+		botoi5.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		center.add(botoi5);
 	}
 
 }
