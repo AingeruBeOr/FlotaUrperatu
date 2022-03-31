65b6464de02c8ef908c351a7bbd6215969315896
Maits27 <maitiurlo@gmail.com>
2022-03-31 17:21:46 +0200

    vh

--- src/eredua/ItsasontziZerrenda.java
+++ src/eredua/ItsasontziZerrenda.java
@@ -1,10 +1,13 @@
 package eredua;
 
+import java.util.Collection;
+import java.util.ArrayList;
+
 public class ItsasontziZerrenda {
 	private Collection<Itsasontzi> lista;
 	
 	public ItsasontziZerrenda() {
-		lista=new Collection<Itsasontzi>();
+		lista=new ArrayList<Itsasontzi>();
 	}
 
 }
