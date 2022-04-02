e5a4719c1ac48803648340777e803506ce61a0e1
Maits27 <maitiurlo@gmail.com>
2022-04-02 19:06:09 +0200

    02/04/2022 19:06

--- src/eredua/Jokalari.java
+++ src/eredua/Jokalari.java
@@ -4,10 +4,19 @@ public abstract class Jokalari {
 	protected float dirua;
 	protected ArmamentuZerrenda armak;
 	protected ItsasontziZerrenda itsasontzi;
+	protected boolean[][] matrizeUkitu;
+	protected boolean[][] matrizeOntzi;
 	public Jokalari ( float pD ) {
+		matrizeUkitu=new boolean[10][10];
+		matrizeOntzi=new boolean[10][10];
 		dirua=pD;
 		armak= new ArmamentuZerrenda();
 		itsasontzi=new ItsasontziZerrenda();
+		for(int i=0; i<10; i++) {
+			for(int j=0; j<10; j++) {
+				matrizeUkitu[i][j]=false;
+			}
+		}
 	}
 	
 	protected ItsasontziZerrenda itsasontziak;
