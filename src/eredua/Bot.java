97ec07f21d55d01f973ec86b931c1cf65ade55b6
Maits27 <maitiurlo@gmail.com>
2022-05-05 17:54:07 +0200

    weiycgqwi

--- src/eredua/Bot.java
+++ src/eredua/Bot.java
@@ -518,10 +518,12 @@ public class Bot extends Jokalari{
 		public void ontziaKonpondu(int x, int y) {
 			if(!nireItsasontziak.urperatutaDago(x,y) && nireItsasontziak.itsasontziaDuGelaxka(x, y) && nireItsasontziak.ukitutaDago(x, y)) {
 				nireItsasontziak.gelaxkaKonpondu(x, y);
+				System.out.println("Hola"+x+" x da eta hau y "+y);
 				setChanged();
 				notifyObservers(new int[] {x,y,11,1});
+				
 			}
-
+			
 		}
 
 }
