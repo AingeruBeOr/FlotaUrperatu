e1476614ce82fa1916aaef38ffc59b1f6f4f77d5
ainho <ainho@10.109.232.232>
2022-05-12 17:23:43 +0200

    Se cierra la tienda y no todo

--- src/bista/Denda.java
+++ src/bista/Denda.java
@@ -93,7 +93,7 @@ public class Denda extends JFrame {
 		initialize();
 	}
 	private void initialize() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 522, 371);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
